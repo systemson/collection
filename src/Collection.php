e9cde3049e02226d32b822830c9c7674c7b53ed3
Systemson <systemson@gmail.com>
2018-11-06 15:19:57 -0400

    Updating docs

--- src/Collection.php
+++ src/Collection.php
@@ -5,8 +5,11 @@ namespace Amber\Collection;
 use Amber\Collection\Base\BaseCollection;
 
 /**
- * @todo Set default configs and settable configs.
- * @todo Add validations.
+ * @todo MUST throw exceptions on argument validations.
+ * @todo SHOULD add support por seaching wildcars. Like: $collection->get('base.{*}.other');
+ *       SHOULD return an array if many items are found, else the matching item.
+ * @todo MUST allow extending classes to set custom multilevel separator. Like '/' instead of '.'
+ * @todo SHOULD consider adding caching ability.
  */
 class Collection extends BaseCollection
 {
