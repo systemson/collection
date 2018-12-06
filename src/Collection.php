75c8bf085814b12644559192c8a371c495024f75
Deivi Peña <systemson@gmail.com>
2018-12-06 12:32:44 -0400

    Style fix

--- src/Collection.php
+++ src/Collection.php
@@ -7,7 +7,7 @@ use Amber\Collection\Base\BaseCollection;
 /**
  * @todo MUST throw exceptions on argument validations.
  * @todo MUST allow to set custom multilevel separator. Like '/' instead of '.'
- *		 SHOULD use a prop to set the separator. 		
+ *       SHOULD use a prop to set the separator.
  * @todo MUST add support for searching wildcars. Like: $collection->get('base.{*}.other');
  *       SHOULD return an array if many items are found, else the matching item.
  * @todo SHOULD consider adding caching ability.
