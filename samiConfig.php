eb2de56776942315ab7d1c1ed84e71b0d25cb0cb
Deivi Peña <systemson@gmail.com>
2019-03-15 10:23:32 -0400

    Minor updates

---
+++ samiConfig.php
@@ -0,0 +1,28 @@
+<?php
+
+use Sami\Sami;
+use Sami\RemoteRepository\GitHubRemoteRepository;
+use Sami\Version\GitVersionCollection;
+use Symfony\Component\Finder\Finder;
+
+$iterator = Finder::create()
+    ->files()
+    ->name('*.php')
+    //->exclude('Resources')
+    //->exclude('Tests')
+    ->in($dir = __DIR__.DIRECTORY_SEPARATOR.'src');
+
+$versions = GitVersionCollection::create($dir)
+    ->addFromTags('v0.3.*')
+    ->addFromTags('v0.4.*')
+    ->add('master', 'master branch');
+
+return new Sami($dir, array(
+    //'theme'                => 'symfony',
+    'versions'             => $versions,
+    'title'                => 'Amber/Collection API',
+    'build_dir'            => __DIR__.'/tmp/build/%version%',
+    'cache_dir'            => __DIR__.'/tmp/cache/sami/%version%',
+    'remote_repository'    => new GitHubRemoteRepository('amber/collection', dirname($dir)),
+    'default_opened_level' => 10,
+));
