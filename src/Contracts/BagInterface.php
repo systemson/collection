e88d687f9762073f229b16c59ab7a24897d18fb8
Deivi Peña <systemson@gmail.com>
2019-05-27 12:20:39 -0400

    Updates

---
+++ src/Contracts/BagInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * This file is part of the Amber/Collection package.
+ *
+ * @package Amber/Collection
+ * @author  Deivi Peña <systemson@gmail.com>
+ * @license GPL-3.0-or-later
+ * @license https://opensource.org/licenses/gpl-license GNU public function License
+ */
+
+namespace Amber\Collection\Contracts;
+
+interface BagInterface extends CollectionInterface
+{
+}
